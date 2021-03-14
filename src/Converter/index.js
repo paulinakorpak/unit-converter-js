b2c8fb9934740b5d6100e14097b1b37050e291ce
Paulina Korpak <korpak.paulina@gmail.com>
2021-03-14 18:21:31 +0000

    Handle calculations

--- src/Converter/index.js
+++ src/Converter/index.js
@@ -4,5 +4,10 @@ export const Converter = () => {
   const getCategories = () => Object.keys(conversions);
   const getUnitsByCategory = (category) => Object.keys(conversions[category]);
 
-  return { getCategories, getUnitsByCategory };
+  const calculate = (category, unitFrom, unitTo, value) => {
+    const result = conversions[category][unitFrom][unitTo](value);
+    return Math.round(result * 10000) / 10000;
+  };
+
+  return { getCategories, getUnitsByCategory, calculate };
 };
--- src/Form/index.js
+++ src/Form/index.js
@@ -3,22 +3,37 @@ import { template } from './template';
 export const Form = (element, converter) => {
   const state = {
     category: 'length',
-    unitFrom: null,
-    unitTo: null,
+    unitFrom: 'centimeter',
+    unitTo: 'meter',
   };
 
   let categorySelect = null;
   let unitSelects = null;
+  let unitFromInput = null;
+  let unitToInput = null;
 
   const changeCategory = (e) => {
     state.category = e.target.value;
-    state.unitFrom = null;
-    state.unitTo = null;
+
+    const units = converter.getUnitsByCategory(state.category);
+
+    state.unitFrom = units[0];
+    state.unitTo = units[1];
     render();
   };
 
   const changeUnit = (e) => {
     state[e.target.dataset.key] = e.target.value;
+    calculate();
+  };
+
+  const calculate = () => {
+    unitToInput.value = converter.calculate(
+      state.category,
+      state.unitFrom,
+      state.unitTo,
+      parseFloat(unitFromInput.value),
+    );
   };
 
   const render = () => {
@@ -30,9 +45,12 @@ export const Form = (element, converter) => {
 
     categorySelect = element.querySelector('.unit-category');
     unitSelects = element.querySelectorAll('.unit-select');
+    unitFromInput = element.querySelector('.unit-from input');
+    unitToInput = element.querySelector('.unit-to input');
 
     categorySelect.addEventListener('change', changeCategory);
     Array.from(unitSelects).map((item) => item.addEventListener('change', changeUnit));
+    unitFromInput.addEventListener('keyup', calculate);
   };
 
   return { render };
